1d1abbda71eaa13538c6026c7843eef7ce8462c6
AdrienSuro <adrien.surowiec@protonmail.com>
2022-11-17 21:20:19 +0100

    Finish regular project

--- script.js
+++ script.js
@@ -1,40 +1,37 @@
 //Fetch the DOM Elements from the HTML : 
 const main = document.querySelector(".main");
 const button = document.querySelector(".userprompt");
-var userInput = 0;
-var i = 0;
-var numberOfSquares = 256;
-var squareWidth = 6;
+var userInput = 16;
+var numberOfSquares = userInput * userInput;
 
 //Define the functions : 
 function createGridElements() {
+    let i = 0;
+    squareWidth = 600 / userInput;
+    numberOfSquares = userInput * userInput;
+    console.log(userInput);
     do 
     {const square = document.createElement('div');
-    square.setAttribute('style', `display:flex; background: white; height: ${squareWidth}% ; width: ${squareWidth}%`);
+    square.setAttribute('style', `display:flex; background: white; height: ${squareWidth}px ; width: ${squareWidth}px`);
     square.addEventListener("mouseover", () => {square.style.backgroundColor = "black";});
     main.appendChild(square);
-    i += 1}
+    i += 1;
+    }
     while (i < numberOfSquares) 
 }
 
 function userInputFunction() {
-    var userInput = parseInt(prompt("Please enter the number of squares you want on each side"));
+    userInput = parseInt(prompt("Please enter the number of squares you want on each side"));
     if (userInput > 100) {
         alert("Common, be kind to your machine, input a number up to 100");
     }
     else if (typeof(userInput) != 'number') {
         alert("Please input a number and not some Schabernack")
     }
-    numberOfSquares = (userInput * userInput)
-    squareWidth = 100 / userInput ;
-    // let allSquares = document.querySelectorAll("div");
-    // for (let j=0; j>allSquares.length; j++) {
-    //     allSquares[0].remove();
-    //     }
     while (main.hasChildNodes()) {
         main.removeChild(main.firstChild);
       }
-    createGridElements()
+    createGridElements();
 }
 
 createGridElements();
